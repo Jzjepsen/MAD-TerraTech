233f2e7388a356830b6cb00a749ccf78b3145513
Andreas Halse Gravesen <andreas.gravesen@lego.com>
2023-11-16 15:50:35 +0100

    made stuff

--- app/src/main/java/com/example/terratech/googleMap.kt
+++ app/src/main/java/com/example/terratech/googleMap.kt
@@ -1,46 +1,75 @@
 package com.example.terratech
 
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.widget.Toast
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.terratech.ui.theme.TerraTechTheme
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class googleMap : ComponentActivity() {
+class googleMap : AppCompatActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         setContent {
-            TerraTechTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting5("Android")
+            MapViewContainer()
+        }
+    }
+
+    @Composable
+    fun MapViewContainer(modifier: Modifier = Modifier) {
+        val mapView = remember {
+            MapView(this).apply {
+                onCreate(Bundle())
+                this.getMapAsync { googleMap ->
+                    MapsInitializer.initialize(this@googleMap)
+                    if (ContextCompat.checkSelfPermission(this@googleMap,
+                            Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                            if (location != null) {
+                                val currentLocation = LatLng(location.latitude, location.longitude)
+                                googleMap.addMarker(MarkerOptions().position(currentLocation).title("You are here"))
+                                googleMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation))
+                            }
+                        }
+                    } else {
+                        ActivityCompat.requestPermissions(this@googleMap,
+                            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+                    }
                 }
             }
         }
-    }
-}
 
-@Composable
-fun Greeting5(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview5() {
-    TerraTechTheme {
-        Greeting5("Android")
+        AndroidView({ mapView }, modifier = modifier)
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
