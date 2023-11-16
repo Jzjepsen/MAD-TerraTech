cfbceb1ac3131d2b77357c016f19bf1722654abc
Caroline <ctlei17@gmail.com>
2023-11-17 00:49:23 +0100

    basic UI components added

--- app/src/main/java/com/example/terratech/manageTerrarium.kt
+++ app/src/main/java/com/example/terratech/manageTerrarium.kt
@@ -3,14 +3,34 @@ package com.example.terratech
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.terratech.ui.theme.TerraTechTheme
+
+
+// TODO: set it so it has "manage <terrarium name>" displayed at the top
+
+
 
 class manageTerrarium : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +42,7 @@ class manageTerrarium : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting4("Android")
+                    Layout("1")
                 }
             }
         }
@@ -30,10 +50,87 @@ class manageTerrarium : ComponentActivity() {
 }
 
 @Composable
-fun Greeting4(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Layout(number: String, modifier: Modifier = Modifier) {
+    Column {
+
+
+        Text(
+            text = "Manage Terrarium $number",
+            modifier = modifier
+        )
+        SwitchTemplate("Heater")
+        SwitchTemplate("Flow Nozzle")
+        SwitchTemplate("Light")
+        ThresholdTemplate("Temperature Threshold")
+        ThresholdTemplate("Humidity Threshold")
+    }
+
+
+
+}
+
+@Composable
+fun SwitchTemplate(label: String) {
+
+    TerraTechTheme {
+        Surface (
+            color = MaterialTheme.colorScheme.secondaryContainer,
+
+        ){
+            Row (
+                modifier = Modifier.padding(horizontal = 20.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            )
+            {
+                Text(text = label)
+                Spacer(modifier = Modifier.width(width = 20.dp))
+                CustomSwitch()
+            }
+        }
+    }
+
+}
+
+@Composable
+fun ThresholdTemplate(label: String){
+    Column(
+        modifier = Modifier.padding(vertical = 8.dp)
+
+    ) {
+        Text(text = label)
+
+
+        Row {
+            TextField("Min")
+            Spacer(modifier = Modifier.width(width = 5.dp))
+            TextField("Max")
+        }
+    }
+}
+
+@Composable
+fun CustomSwitch() {
+    var checked by remember { mutableStateOf(true) }
+    Switch(
+        checked = checked,
+        onCheckedChange = {
+            checked = it
+        }
+
+    )
+}
+
+
+//TODO: restrict to numbers, restrict the use of enter
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TextField(placeholderText: String) {
+    var text by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text(placeholderText) },
     )
 }
 
@@ -41,6 +138,6 @@ fun Greeting4(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview4() {
     TerraTechTheme {
-        Greeting4("Android")
+        Layout("Android")
     }
 }
