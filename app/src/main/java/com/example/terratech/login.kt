0ba2beaad1e0741799f2d6c5a260289ba3985ba9
Søren Mehlsen <78858933+soerenmehlsen@users.noreply.github.com>
2023-11-16 15:17:27 +0100

    Started on the loginscreen

--- app/src/main/java/com/example/terratech/login.kt
+++ app/src/main/java/com/example/terratech/login.kt
@@ -3,14 +3,44 @@ package com.example.terratech
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.terratech.ui.theme.Purple40
 import com.example.terratech.ui.theme.TerraTechTheme
+import com.google.android.gms.base.R
 
 class login : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,10 +67,41 @@ fun Greeting6(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SignIn(modifier: Modifier = Modifier){
+    val text: MutableState<String> = remember { mutableStateOf("") }
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    )
+    {
+        Icon(
+            imageVector = Icons.Filled.AccountCircle,
+            contentDescription = "Account image",
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(16.dp,4.dp)
+        )
+
+        Spacer(modifier = Modifier
+            .fillMaxWidth()
+            .padding(12.dp))
+
+        OutlinedTextField(
+            value = text.value,
+            onValueChange = {text.value = it},
+            label = { Text("Username") }
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview6() {
     TerraTechTheme {
-        Greeting6("Android")
+        SignIn()
     }
 }
