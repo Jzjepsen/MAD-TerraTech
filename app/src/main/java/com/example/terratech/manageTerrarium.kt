271b51276fc911735f539941a72d5443b4ed9240
ctlei <ctlei17@gmail.com>
2023-11-23 13:12:22 +0100

    updated manager UI

--- app/src/main/java/com/example/terratech/manageTerrarium.kt
+++ app/src/main/java/com/example/terratech/manageTerrarium.kt
@@ -1,36 +1,44 @@
 package com.example.terratech
 
+import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.terratech.ui.theme.TerraTechTheme
-
-
-// TODO: set it so it has "manage <terrarium name>" displayed at the top
-
-
 
 class manageTerrarium : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,19 +59,24 @@ class manageTerrarium : ComponentActivity() {
 
 @Composable
 fun Layout(number: String, modifier: Modifier = Modifier) {
-    Column {
 
+    TerraTechTheme {
+            Box(
+                contentAlignment = Alignment.Center
+            ) {
+                Column {
 
-        Text(
-            text = "Manage Terrarium $number",
-            modifier = modifier
-        )
-        SwitchTemplate("Heater")
-        SwitchTemplate("Flow Nozzle")
-        SwitchTemplate("Light")
-        ThresholdTemplate("Temperature Threshold")
-        ThresholdTemplate("Humidity Threshold")
+                    SwitchTemplate("Heater")
+                    SwitchTemplate("Flow Nozzle")
+                    SwitchTemplate("Light")
+
+                    ThresholdTemplate("Temperature Threshold")
+                    ThresholdTemplate("Humidity Threshold")
+                }
+            }
+
     }
+
 
 
 
@@ -73,39 +86,74 @@ fun Layout(number: String, modifier: Modifier = Modifier) {
 fun SwitchTemplate(label: String) {
 
     TerraTechTheme {
-        Surface (
-            color = MaterialTheme.colorScheme.secondaryContainer,
+        Box (
+            modifier = Modifier
+                .padding(horizontal = 60.dp)
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Surface(
+                color = MaterialTheme.colorScheme.secondaryContainer,
+                shape = RoundedCornerShape(10.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(60.dp)
 
-        ){
-            Row (
-                modifier = Modifier.padding(horizontal = 20.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
-            )
-            {
-                Text(text = label)
-                Spacer(modifier = Modifier.width(width = 20.dp))
-                CustomSwitch()
+
+            ) {
+                Row(
+                    modifier = Modifier.padding(horizontal = 20.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                )
+                {
+                    Text(text = label,
+                        style = MaterialTheme.typography.bodyLarge.copy(fontSize = 18.sp))
+                    Spacer(modifier = Modifier.width(width = 20.dp))
+                    CustomSwitch()
+                }
             }
+
         }
+        Spacer(modifier = Modifier.padding(vertical = 5.dp))
     }
+
 
 }
 
 @Composable
 fun ThresholdTemplate(label: String){
-    Column(
-        modifier = Modifier.padding(vertical = 8.dp)
 
-    ) {
-        Text(text = label)
+        Column(
+            modifier = Modifier
+                .padding(vertical = 8.dp)
+                .fillMaxWidth(),
 
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = label,
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 18.sp)
+            )
 
-        Row {
-            TextField("Min")
-            Spacer(modifier = Modifier.width(width = 5.dp))
-            TextField("Max")
+            Row {
+                NumberInputTemplate(label = "Min.")
+                Spacer(modifier = Modifier.width(width = 5.dp))
+                NumberInputTemplate(label = "Max.")
+            }
         }
+
+}
+
+@Composable
+fun NumberInputTemplate (label: String){
+    Box (modifier = Modifier.width(100.dp)) {
+        StylisedTextField(placeholderText = label)
     }
+
+
 }
 
 @Composable
@@ -121,18 +169,23 @@ fun CustomSwitch() {
 }
 
 
-//TODO: restrict to numbers, restrict the use of enter
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TextField(placeholderText: String) {
+fun StylisedTextField(placeholderText: String) {
     var text by remember { mutableStateOf("") }
 
     OutlinedTextField(
         value = text,
         onValueChange = { text = it },
         label = { Text(placeholderText) },
+        singleLine = true,
+        textStyle = TextStyle(fontSize = 20.sp),
+        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Decimal)
     )
 }
+
+ 
 
 @Preview(showBackground = true)
 @Composable
