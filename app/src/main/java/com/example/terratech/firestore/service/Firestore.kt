e64aa74c075e9b89282418c2be2fea79154b6051
Søren Mehlsen <78858933+soerenmehlsen@users.noreply.github.com>
2023-11-23 13:39:51 +0100

    Fixed a error when trying to login with wrong information

--- app/src/main/java/com/example/terratech/firestore/service/Firestore.kt
+++ app/src/main/java/com/example/terratech/firestore/service/Firestore.kt
@@ -23,7 +23,7 @@ class Firestore(private val api: FirebaseFirestore, private val auth: FirebaseAu
                     } else {
                         // If sign in fails, display a message to the user.
                         Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                        continuation.resume(task.exception ?: Exception("Login failed"))
+                        throw throw Exception("createUserWithEmail: $email failure", task.exception)
                     }
                 }
         }
