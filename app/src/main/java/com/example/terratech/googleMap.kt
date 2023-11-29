bce7c880fa40c8568e245d619f7bae8f6d3e021e
Kamilla Stauenberg L Halkjær <au681524@uni.au.dk>
2023-11-29 21:50:17 +0100

    google maps changes

--- app/src/main/java/com/example/terratech/googleMap.kt
+++ app/src/main/java/com/example/terratech/googleMap.kt
@@ -1,6 +1,8 @@
 package com.example.terratech
 
 import android.Manifest
+import android.app.Activity
+import android.content.Context
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
@@ -56,15 +58,22 @@ import androidx.compose.material.Text
 import androidx.compose.ui.unit.sp
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Column
-
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.platform.LocalContext
+import com.example.terratech.ui.theme.TerraTechTheme
+import com.google.android.gms.maps.GoogleMap
 
 
 class googleMap : AppCompatActivity() {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var mapView: MapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        mapView = MapView(this)
+        mapView.onCreate(savedInstanceState)
         setContent {
             TerraTechTheme {
                 val intentHome = Intent(this@googleMap, listOfTerrariums::class.java)
@@ -82,77 +91,164 @@ class googleMap : AppCompatActivity() {
                     }
                 }
             }
-            MapViewContainer()
+            MapScreen()
         }
     }
 
     @Composable
-    fun MapViewContainer() {
-        val mapView = remember {
-            MapView(this).apply {
-                onCreate(Bundle())
-                this.getMapAsync { googleMap ->
-                    MapsInitializer.initialize(this@googleMap)
-                    if (ContextCompat.checkSelfPermission(this@googleMap,
-                            Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
-                            if (location != null) {
-                                val currentLocation = LatLng(location.latitude, location.longitude)
-                                googleMap.addMarker(MarkerOptions().position(currentLocation).title("You are here"))
-                                googleMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation))
-                            }
-                        }
-                    } else {
-                        ActivityCompat.requestPermissions(this@googleMap,
-                            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
-                    }
+    fun MyTopBar(context: Context) {
+        TopAppBar(
+            title = { Text("Google Maps Overview", color = Color.White, fontSize = 18.sp) },
+            backgroundColor = Color.DarkGray,
+            actions = {
+                IconButton(onClick = {
+                    context.startActivity(Intent(context, listOfTerrariums::class.java))
+                }) {
+                    Icon(Icons.Filled.Home, contentDescription = "Home")
+                }
+            },
+            navigationIcon = {
+                IconButton(onClick = {
+                    (context as? Activity)?.finish() // Casting the context to Activity to call finish
+                }) {
+                    Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
                 }
             }
+        )
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun MapScreen() {
+        val context = LocalContext.current // Getting the context
+        Scaffold(
+            topBar = { MyTopBar(context) }
+        ) { paddingValues ->
+            MapViewContainer(mapView, paddingValues)
         }
+    }
 
-        Column {
-            // Adding title text at the top of the screen
-            TopAppBar(
-                title = { Text("Google Maps Overview", color = Color.White, fontSize = 18.sp) },
-                backgroundColor = Color.DarkGray,
-                modifier = Modifier.padding(top = 16.dp)
-            )
-
+    @Composable
+    fun MapViewContainer(mapView: MapView, paddingValues: PaddingValues) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
             Box(
-                modifier = Modifier.fillMaxWidth().padding(16.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
                 contentAlignment = Alignment.Center,
-                content = {
-                    Surface(color = Color.DarkGray, modifier = Modifier.padding(8.dp)) {
-                        Text(
-                            text = "Terrarium Coordinates:\nLat: 56°08'40.8\"N\nLong: 10°11'06.5\"E",
-                            color = Color.White,
-                            fontSize = 18.sp,
-                            modifier = Modifier.padding(8.dp)
-                        )
-                    }
+            ) {
+                Surface(color = Color.DarkGray, modifier = Modifier.padding(8.dp)) {
+                    Text(
+                        text = "Phone Coordinates:\nLat: 56°08'40.8\"N\nLong: 10°11'06.5\"E",
+                        color = Color.White,
+                        fontSize = 18.sp,
+                        modifier = Modifier.padding(8.dp)
+                    )
                 }
-            )
+            }
 
-            Box(modifier = Modifier.fillMaxSize().offset(y = (-100).dp), contentAlignment = Alignment.Center) {
-                AndroidView({ mapView }, modifier = Modifier.fillMaxWidth(0.7f).fillMaxHeight(0.4f))
+            AndroidView(
+                { mapView },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+            ) { mapView ->
+                mapView.getMapAsync { googleMap ->
+                    setupGoogleMap(googleMap)
+                }
             }
         }
     }
 
 
+    private fun setupGoogleMap(googleMap: GoogleMap) {
+        MapsInitializer.initialize(this@googleMap)
+        if (ContextCompat.checkSelfPermission(
+                this@googleMap,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    val currentLocation = LatLng(location.latitude, location.longitude)
+                    googleMap.addMarker(
+                        MarkerOptions().position(currentLocation).title("You are here")
+                    )
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation))
+                }
+            }
+        } else {
+            ActivityCompat.requestPermissions(
+                this@googleMap,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1
+            )
+        }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
         if (requestCode == 1) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                if (ContextCompat.checkSelfPermission(this,
-                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) {
                     Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+                    mapView.getMapAsync { googleMap ->
+                        googleMap.isMyLocationEnabled = true
+                        // Add additional setup if required
+                    }
                 }
             } else {
                 Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
             }
         }
     }
-
 }
